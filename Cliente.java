e1a258c9ca7cd14d6e963eafa4598d46a540c99c
Maria Fernanda <161635111+MafeFaria@users.noreply.github.com>
2024-04-15 17:02:12 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,57 @@
+public class Cliente {
+
+    private String nome;
+    private String cpf;
+    private String endereco;
+    private int idade;
+    private char sexo;
+
+    // Getter para o atributo 'nome'
+    public String getNome() {
+        return nome;
+    }
+
+    // Setter para o atributo 'nome'
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    // Getter para o atributo 'cpf'
+    public String getCpf() {
+        return cpf;
+    }
+
+    // Getter para o atributo 'endereco'
+    public String getEndereco() {
+        return endereco;
+    }
+
+    // Setter para o atributo 'endereco'
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    // Getter para o atributo 'idade'
+    public int getIdade() {
+        return idade;
+    }
+
+    // Setter para o atributo 'idade'
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    // Getter para o atributo 'sexo'
+    public char getSexo() {
+        return sexo;
+    }
+
+    // Setter para o atributo 'sexo'
+    public void setSexo(char sexo) {
+        this.sexo = sexo;
+    }
+}
+
+
+
+
